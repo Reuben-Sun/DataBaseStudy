494f9793b41439ef205667984388cc7f51991b90
sunzheng <2824016820@qq.com>
2021-12-21 23:27:13 +0800

    打分

--- src/main/java/com/reuben/databasestudy/controller/ScoreController.java
+++ src/main/java/com/reuben/databasestudy/controller/ScoreController.java
@@ -1,10 +1,39 @@
 package com.reuben.databasestudy.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.reuben.databasestudy.entity.Score;
+import com.reuben.databasestudy.mapper.ScoreMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/score")
 public class ScoreController {
+    @Autowired
+    ScoreMapper scoreMapper;
 
+    @GetMapping("/selectAll")
+    List<Score> selectAll(){
+        List<Score> scores = scoreMapper.selectAll();
+        return scores;
+    }
+
+    @GetMapping("/selectByAthlete")
+    Score selectByAthlete(int athleteId, int eventId){
+        Score score = scoreMapper.selectByAthlete(athleteId, eventId);
+        return score;
+    }
+
+    @GetMapping("/deleteScore")
+    int deleteScore(int id){
+        int i = scoreMapper.deleteScore(id);
+        return i;
+    }
+
+    @PostMapping("/insertScore")
+    int insertScore(@RequestBody Score score){
+        int i = scoreMapper.addScore(score);
+        return i;
+    }
 }
